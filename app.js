1b915f73f48e10288e6ef79bfb3e305a234cb891
cphil31 <cecile_philippe@live.fr>
2016-09-30 15:43:03 +0200

    exercice fait

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('have fun !');
+
+$("button").click (function(){
+//recuperer la valeur des champs
+var space={
+	//selectionner un champ
+	prenom : $("#first_name").val(),
+	nom : $("#last_name").val(),
+	city : $("#city").val(),
+}
+
+$("#username").text(space.prenom + " " + space.nom);
+
+
+console.log(space)
+
+
+});
+
